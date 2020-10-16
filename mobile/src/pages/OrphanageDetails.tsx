3c40db307707f5e5b91b9d0cd60527251eea42d4
VitoriaCarvalho <vitoriiacb@gmail.com>
2020-10-16 14:40:31 -0300

    Aula 4 - mobile app iniciado

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function OrphanageDetails() {
+    return (
+        <View>
+            <Text>Chegou na página OrphanageDetail</Text>
+        </View>
+    )
+}
